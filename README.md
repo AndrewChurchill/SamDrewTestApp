5832b82a10a23389d31ca2137c0a980f381d59d2
Drew Churchill <dmc9870@gmail.com>
2022-02-28 21:47:30 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SamDrewTestApp
+
+Just a repo for testing things out. Nothing to see here.
+
+![Obi Wan](Assets/obi-wan.jpg)
---
+++ SamDrewTestApp.Server/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SamDrewTestApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await Task.FromResult("HELLO, NEW YORK!!!"));
+        }
+    }
+}
